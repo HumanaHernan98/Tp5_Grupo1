620af536f26ecb72f62a0f1c8c6b42acbf96b872
HUMANA  GUSTAVO <HUMANA  GUSTAVO@LAPTOP-2M34BLRB>
2022-05-21 16:19:00 -0300

    agregado de la clase alumno controller modificacion en el formulario alumnos

---
+++ Tp5_Grupo1/src/main/java/ar/edu/unju/fi/controller/AlumnoController.java
@@ -0,0 +1,30 @@
+package ar.edu.unju.fi.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import model.Alumno;
+
+@Controller
+public class AlumnoController {
+	List<Alumno> listaAlumno= new ArrayList<>();
+	Alumno unAlumno = new Alumno(); 
+	@GetMapping("/alumno")
+	public String getAlumno(Model model) {
+		model.addAttribute("alumno",unAlumno);
+		return("nuevo_alumnos");
+	}
+	
+	@PostMapping("/alumno/guardar")
+	public String guardarAlumno(@ModelAttribute("alumno")Alumno unAlumno,Model model) {
+		listaAlumno.add(unAlumno);
+		model.addAttribute("alumnos", listaAlumno);
+		return("lista_alumno");
+	}
+}
