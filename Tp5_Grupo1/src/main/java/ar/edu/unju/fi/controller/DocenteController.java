4573bc8fa26730bd7bf444a4f124a7f72a3a45fa
gusta <gusta@LAPTOP-P47OBGCS>
2022-06-08 11:21:28 -0300

    Agregado de registro de trazas. redireccion de datos tomados por formularios. mostrar contenidos de tabla y cargar nuevos sujetos.

--- Tp5_Grupo1/src/main/java/ar/edu/unju/fi/controller/DocenteController.java
+++ Tp5_Grupo1/src/main/java/ar/edu/unju/fi/controller/DocenteController.java
@@ -1,13 +1,42 @@
 package ar.edu.unju.fi.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import model.Docente;
 
 @Controller
 public class DocenteController {
+	Logger logger = LoggerFactory.getLogger(CursosController.class);
+	List<Docente> listaDocente = new ArrayList<>();
+	Docente unDocente = new Docente();
 	@GetMapping("/ListaD")
 	public String getIndexPage(Model model) {
+		logger.info("REQUEST: /ListaD - METHOD: getListaDocentes() - INFO: Se solicita mostrar en contenido de la lista de docentes");
 		return "lista_docentes";
 	}
+	
+	
+	@GetMapping("/docente")
+	public String getFormDocente(Model model) {
+		model.addAttribute("docente", unDocente);
+		logger.info("REQUEST: /curso - METHOD: getFormDocente() - INFO: Se solicita cargar un nuevo objeto docente, se devuelve el formulario");
+		return ("nuevo_docente");
+	}
+	
+	@PostMapping("/docente/guardar")
+	public String guardarDocente(@ModelAttribute("docente")Docente unDocente,Model model) {
+		listaDocente.add(unDocente);
+		model.addAttribute("docentes", listaDocente);
+		logger.info("REQUEST: /docente/guardar - METHOD: guardardocente() - INFO: Se agrego un nuevo objeto docente a la lista de docente, se devuelve el la tabla actualizada");		return ("lista_docentes");
+	}
+	
 }
